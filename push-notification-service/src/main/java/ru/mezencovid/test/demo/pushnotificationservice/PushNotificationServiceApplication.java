5701b51f4993e364de19c5f8ad6461f0daaf181a
Igor-Dmitrievich <mezentsovigor@gmail.com>
2023-01-23 21:47:20 +0400

    Создал клиента Eureka-сервера -> push-notification-service

---
+++ push-notification-service/src/main/java/ru/mezencovid/test/demo/pushnotificationservice/PushNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.mezencovid.test.demo.pushnotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ push-notification-service/src/test/java/ru/mezencovid/test/demo/pushnotificationservice/PushNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mezencovid.test.demo.pushnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
