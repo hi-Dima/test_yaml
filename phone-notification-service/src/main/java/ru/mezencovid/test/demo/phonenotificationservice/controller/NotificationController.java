70c4922e2e435a2aba0688af1c752a48e71975c4
Igor-Dmitrievich <mezentsovigor@gmail.com>
2023-01-24 06:54:48 +0400

    Добавил модули в общие pom проекта, написал readmi-файл, поменял методы запросов у меодов микросервисов

--- phone-notification-service/src/main/java/ru/mezencovid/test/demo/phonenotificationservice/controller/NotificationController.java
+++ phone-notification-service/src/main/java/ru/mezencovid/test/demo/phonenotificationservice/controller/NotificationController.java
@@ -2,6 +2,7 @@ package ru.mezencovid.test.demo.phonenotificationservice.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/phone")
 public class NotificationController {
 
-    @GetMapping("/notification")
+    @PostMapping("/notification")
     public String notification(){
         log.info("sms was sent");
         return "Чекай логи, уведомление отпралено!";
